26ba650a2bed622e7d2c33e52a0bc7053dad0435
vishalenrique <vishal3bhati@gmail.com>
2017-02-25 17:13:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/com/my/BlankFragment.java
@@ -0,0 +1,88 @@
+package com.example.com.my;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BlankFragment extends Fragment {
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.v("onActivityCreated","onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.v("onStart","onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v("onResume","onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.v("onPause","onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.v("onStop","onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("onDestroy","onDestroy");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.v("onDestroyView","onDestroyView");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v("onCreate","onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        Log.v("onCreateView","onCreateView");
+        return textView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.v("onAttach","onAttach");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.v("onDetach","onDetach");
+    }
+}
---
+++ app/src/main/java/com/example/com/my/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.com.my;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("onCreate Activity","onCreate Activity");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("onStart Activity","onStart Activity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("onPause Activity","onPause Activity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("onStop Activity","onStop Activity");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("onRestart Activity","onRestart Activity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("onResume  Activity","onResume  Activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("onDestroy Activity","onDestroy Activity");
+    }
+}
